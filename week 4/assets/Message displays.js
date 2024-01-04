67010bd0ea10d682c91a9792e2b86e944390b5fc
gomuningen2004 <govardhan.personal.new@gmail.com>
2024-01-04 22:04:27 +0530

    separed the html, css and javascript components of the code

---
+++ week 4/assets/Message displays.js
@@ -0,0 +1,5 @@
+document.getElementById("demo").innerHTML = "This is a message written using inner Html";
+document.write("This is a statement displayed using document.write");
+alert("This is a statement displayed using alert");
+window.alert("This is a statement displayed using window.alert");
+console.log("This is a statement displayed using console.log");
---
+++ week 4/assets/Strings.js
@@ -0,0 +1,15 @@
+let originalString = "Hello, World!";
+let concatString = originalString.concat(" How are you?");
+let subString = originalString.substring(0, 5);
+let charAtIndex = originalString.charAt(7);
+let lowerCaseString = originalString.toLowerCase();
+let upperCaseString = originalString.toUpperCase();
+let stringLength = originalString.length;
+
+console.log("Original String:", originalString);
+console.log("Concatenated String:", concatString);
+console.log("Substring:", subString);
+console.log("Character at Index 7:", charAtIndex);
+console.log("Lowercase String:", lowerCaseString);
+console.log("Uppercase String:", upperCaseString);
+console.log("String Length:", stringLength);
