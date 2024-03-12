fcc14f01e16d85e4bbdd408d39222577876b97f9
gomuningen2004 <govardhan.personal.new@gmail.com>
2024-03-12 20:40:15 +0530

    attempt of jdbc for backend

---
+++ jdbc/src/jdbc.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+public class jdbc {
+    public static final String url = "jdbc:mysql://localhost:3306/student";
+    public static final String username = "root";
+    public static final String password = "go123";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            Statement stmt = con.createStatement();
+            String query = "select * from student";
+
+            ResultSet result = stmt.executeQuery(query);
+            System.out.println("Name    | Age | Branch | Marks");
+            System.out.println("--------------------------------");
+            while (result.next()) {
+                String name = result.getString("name");
+                int age = result.getInt("age");
+                String branch = result.getString("branch");
+                int marks = result.getInt("marks");
+
+                // Using formatting to align columns
+                System.out.printf("%-8s| %-4d| %-7s| %-5d%n", name, age, branch, marks);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
