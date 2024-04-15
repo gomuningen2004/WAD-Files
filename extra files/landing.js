f92f985c3ac1c1dfcab76ddb4849350b49944576
gomuningen2004 <govardhan.personal.new@gmail.com>
2024-04-15 14:36:15 +0530

    removed few files

--- extra files/landing.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("signIn").addEventListener("click", function() {
-  document.querySelector(".container").classList.remove("right-panel-active");
-});
-
-document.getElementById("signUp").addEventListener("click", function() {
-  document.querySelector(".container").classList.add("right-panel-active");
-});
